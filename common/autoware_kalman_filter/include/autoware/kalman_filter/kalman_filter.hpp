5083bdfe42288914d836c1edc4b30000f4c96963
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2025-04-01 16:40:39 +0900

    Update common/autoware_kalman_filter/include/autoware/kalman_filter/kalman_filter.hpp

--- common/autoware_kalman_filter/include/autoware/kalman_filter/kalman_filter.hpp
+++ common/autoware_kalman_filter/include/autoware/kalman_filter/kalman_filter.hpp
@@ -130,7 +130,7 @@ public:
    * @param i index of kalman filter state
    * @return value of i's component of the kalman filter state x[i]
    */
-  [[nodiscard]] double getXelement(unsigned int i) const;
+  double getXelement(unsigned int i) const;
 
   /**
    * @brief calculate kalman filter state and covariance by prediction model with A, B, Q matrix.
